7ae68d055df6c2eafae3263f7246bde42342dcb8
hari-02006 <harishkhajuria76@gmail.com>
2025-05-10 13:07:20 +0530

    Update mk.py

--- mk.py
+++ mk.py
@@ -148,4 +148,4 @@ def index():
     return render_template('index.html', token_info=token_info)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000,debug=True)
+    app.run(debug=True)
